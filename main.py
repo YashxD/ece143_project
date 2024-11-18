3f013e6936a508d05087256797592dc1d09d20fa
Mew463 <72902803+Mew463@users.noreply.github.com>
2024-11-18 15:42:08 -0800

    adding locations

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta, time
+from functions import *
 
 # Configure the page
 st.set_page_config(page_title="Multi-Page App", page_icon="📚", layout="wide")
 
 # Sidebar navigation
-st.sidebar.title("Navigation")
+st.sidebar.title("Pages")
 
 # Define available pages
 pages = {
     "Theft_Map": "Theft Map",
     "PricevsTOD": "Price Versus Time of Day",
-    "WeathervsTheft": "Weather Versus Theft"
+    "WeathervsTheft": "Weather Versus Theft",
+    "BicyclesVsScooters" : "Bicycles Versus Scooters",
+    "Worstday" : " Who had the worst day?"
 }
 
 # Initialize session state to remember the current page
 if "current_page" not in st.session_state:
-    st.session_state.current_page = "Home"
+    st.session_state.current_page = "Theft_Map"
 
 # Create clickable buttons for each page
 for page_name, page_display_name in pages.items():
@@ -28,19 +31,19 @@ for page_name, page_display_name in pages.items():
 # Display the selected page content
 selected_page = st.session_state.current_page
 
-def examplegetfunction(start_date, end_date, from_TOD, to_TOD):
-    return pd.DataFrame(np.random.randn(1000, 2) / [250, 250] + [32.8812, -117.2344], columns=["lat", "lon"])
-
 if selected_page == "Theft_Map":
     st.title("Theft Map")
     
     end_date = datetime(2024,11,12)
     start_date = end_date - timedelta(days=5 * 365)  # Approximate 5 years back
-    x = st.slider('Dates From', min_value= start_date, max_value=end_date, value=(start_date, end_date), format="YYYY-MM-DD")  # 👈 this is a widget
-    y = st.slider('From (Time of day)', min_value=time(0,0), max_value=time(23,59), value = (time(0,0)))
-    z = st.slider('To (Time of day)', min_value=time(0,0), max_value=time(23,59), value = (time(23,59)))
+    dateRange = st.slider('Dates From', min_value= start_date, max_value=end_date, value=(start_date, end_date), format="MM/DD/YYYY") 
+    FromTOD = st.slider('From (Time of day)', min_value=time(0,0), max_value=time(23,59), value = (time(0,0)))
+    ToTOD = st.slider('To (Time of day)', min_value=time(0,0), max_value=time(23,59), value = (time(23,59)))
+    options = ['bicycle', 'scooter', 'skateboard']
+    selected_options = st.multiselect('Type of PEV:', options)
+    coordinates = mapPlot(dateRange[0], dateRange[1], FromTOD, ToTOD,  selected_options)
+    st.map(coordinates, size="0.5")
     
-    st.map(examplegetfunction(1,1,1,1), size=3)
 
 elif selected_page == "PricevsTOD":
     st.title("Map Page")
